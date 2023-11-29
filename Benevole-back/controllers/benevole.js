14660f06e3f4e425bfc92e70919065add3f609ee
Auriane <aurianepoirier@MacBook-Pro-de-Auriane.local>
2023-11-29 17:56:42 +0100

    je fais des test

--- Benevole-back/controllers/benevole.js
+++ Benevole-back/controllers/benevole.js
@@ -33,11 +33,11 @@ exports.modifyBenevole = (req, res, next) => {
     );
   };
 
-// exports.deleteBenevole = (req, res, next) => {
-//     Benevole.deleteOne({_id: req.params.id})
-//     .then(() => {res.status(200).json({message: 'Benevole supprimé !'})})
-//     .catch((error) => {res.status(400).json({error: error})})
-// };
+exports.deleteBenevole = (req, res, next) => {
+    Benevole.deleteOne({_id: req.params.id})
+    .then(() => {res.status(200).json({message: 'Benevole supprimé !'})})
+    .catch((error) => {res.status(400).json({error: error})})
+};
 
 exports.getAllBenevole = (req, res, next) => {
     Benevole.find()
