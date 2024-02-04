803e7e6de01f86c946e3b9a31044c0be701e55e7
Auriane Poirier <poirierauriane@gmail.com>
2024-02-04 21:10:36 +0100

    test modif app

--- Benevole-back/app.js
+++ Benevole-back/app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-app.use(cors());
 
 const benevoleRoutes = require('./routes/benevole');
 const animationRoutes = require('./routes/animation');
@@ -16,20 +15,25 @@ const festivalRoutes = require('./routes/festival');
 const flexibleRoutes = require('./routes/flexible');
 const flexibleZoneRoutes = require('./routes/flexibleZone');
 
+const corsOptions = {
+    origin: '*', // Remplacez '*' par vos domaines spécifiques si besoin
+    optionsSuccessStatus: 200
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL,
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée...'));
 
-app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Accès à l'API depuis n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Ajout des headers mentionnés aux requêtes envoyées vers notre API
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Envoi de requêtes avec les méthodes mentionnées
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
